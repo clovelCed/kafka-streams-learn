2737f4fdeafd28f10dd70b351e6807a2587e79a9
Cédric <cedric.clovel@gmail.com>
2023-03-19 01:41:46 +0100

    Update data to forbes companies

--- forbes-producer/src/main/java/fr/viteducode/kafkaclient/producer/simple/ForbesProducerApplication.java
+++ forbes-producer/src/main/java/fr/viteducode/kafkaclient/producer/simple/ForbesProducerApplication.java
@@ -7,6 +7,9 @@ import com.opencsv.RFC4180ParserBuilder;
 import com.opencsv.enums.CSVReaderNullFieldIndicator;
 import fr.viteducode.kafkaclient.producer.simple.data.BillionaireKey;
 import fr.viteducode.kafkaclient.producer.simple.data.BillionaireValue;
+import fr.viteducode.kafkaclient.producer.simple.data.CompanieKey;
+import fr.viteducode.kafkaclient.producer.simple.data.CompanieValue;
+import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -17,11 +20,13 @@ import java.util.Properties;
 
 public class ForbesProducerApplication {
 
-    private final static String TOPIC = "topic-billionaires";
+    private final static String TOPIC_BILLIONAIRE = "topic-billionaires";
+
+    private final static String TOPIC_COMPANIE = "topic-companies";
 
     private final RFC4180Parser rfc4180ParserBuilder;
 
-    private KafkaProducer<BillionaireKey, BillionaireValue> producer;
+    private KafkaProducer<SpecificRecord, SpecificRecord> producer;
 
     public ForbesProducerApplication() {
 
@@ -44,6 +49,13 @@ public class ForbesProducerApplication {
     }
 
     public void run() {
+        this.produceBillionaire();
+        this.produceCompanie();
+        producer.close();
+    }
+
+    private void produceBillionaire() {
+
         try (InputStream fileStream = ForbesProducerApplication.class.getClassLoader().getResourceAsStream("forbes_billionaires_2022.csv");
              InputStreamReader inputStreamReader = new InputStreamReader(fileStream);
              CSVReader csvReader = new CSVReaderBuilder(inputStreamReader).withCSVParser(rfc4180ParserBuilder).withSkipLines(1).build()) {
@@ -51,10 +63,10 @@ public class ForbesProducerApplication {
             String[] line;
             while ((line = csvReader.readNext()) != null) {
 
-                BillionaireKey key = this.mapKey(line);
-                BillionaireValue value = this.mapValue(line);
+                BillionaireKey key = this.mapBillionaireKey(line);
+                BillionaireValue value = this.mapBillionaireValue(line);
 
-                ProducerRecord<BillionaireKey, BillionaireValue> record = new ProducerRecord<>(TOPIC, key, value);
+                ProducerRecord<SpecificRecord, SpecificRecord> record = new ProducerRecord<>(TOPIC_BILLIONAIRE, key, value);
                 producer.send(record, (recordMetadata, exception) -> {
                     if (exception != null) {
                         System.out.println(exception.getMessage());
@@ -64,11 +76,33 @@ public class ForbesProducerApplication {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        producer.close();
     }
 
-    private BillionaireValue mapValue(String[] line) {
+    private void produceCompanie() {
+
+        try (InputStream fileStream = ForbesProducerApplication.class.getClassLoader().getResourceAsStream("forbes_companies_2022.csv");
+             InputStreamReader inputStreamReader = new InputStreamReader(fileStream);
+             CSVReader csvReader = new CSVReaderBuilder(inputStreamReader).withCSVParser(rfc4180ParserBuilder).withSkipLines(1).build()) {
+
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+
+                CompanieKey key = this.mapCompanieKey(line);
+                CompanieValue value = this.mapCompanieValue(line);
+
+                ProducerRecord<SpecificRecord, SpecificRecord> record = new ProducerRecord<>(TOPIC_COMPANIE, key, value);
+                producer.send(record, (recordMetadata, exception) -> {
+                    if (exception != null) {
+                        System.out.println(exception.getMessage());
+                    }
+                });
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BillionaireValue mapBillionaireValue(String[] line) {
         return BillionaireValue.newBuilder()
                 .setName(line[1])
                 .setAge(this.parseInt(line[2]))
@@ -81,8 +115,23 @@ public class ForbesProducerApplication {
                 .build();
     }
 
-    private BillionaireKey mapKey(String[] line) {
+    private CompanieValue mapCompanieValue(String[] line) {
+        return CompanieValue.newBuilder()
+                .setName(line[1])
+                .setCountry(line[2])
+                .setSales(line[3])
+                .setProfit(line[4])
+                .build();
+    }
+
+    private BillionaireKey mapBillionaireKey(String[] line) {
         return BillionaireKey.newBuilder()
+                .setId(this.parseInt(line[0]))
+                .build();
+    }
+
+    private CompanieKey mapCompanieKey(String[] line) {
+        return CompanieKey.newBuilder()
                 .setId(this.parseInt(line[0]))
                 .build();
     }
